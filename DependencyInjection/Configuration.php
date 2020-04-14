2873f290081bf1b77de071cda622b90cb09b719d
Arif Allahverdiev <cheaterby@gmail.com>
2020-04-14 19:12:58 +0300

    update for SF5

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('google_analytics_api');
-
         $treeBuilder
             ->getRootNode()
             ->children()
